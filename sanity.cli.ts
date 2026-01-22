16b682b4fa131f31efa5780b626eb2ab04318a9d
Gary Taylor <gball914@gmail.com>
2026-01-22 16:20:19 -0500

    Fix Sanity CLI api config

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -2,4 +2,9 @@ import { defineCliConfig } from "sanity/cli"
 
 import { dataset, projectId } from "./sanity/lib"
 
-export default defineCliConfig({ project: projectId, dataset })
+export default defineCliConfig({
+  api: {
+    projectId,
+    dataset,
+  },
+})
