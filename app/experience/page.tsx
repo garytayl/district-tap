a7be2b2ac1df88ae23e72f8e34106370c396fcb2
Gary Taylor <gball914@gmail.com>
2026-01-28 05:16:21 -0500

    Fix button structure in mobile experience page by replacing a misplaced closing tag for improved layout consistency.

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -323,7 +323,7 @@ export default function MobileExperience() {
                       Drinks Menu
                     </Button>
                   </div>
-                </button>
+                </div>
               </div>
             ) : null}
 
